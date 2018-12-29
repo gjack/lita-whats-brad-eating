1bf95c0370c7a459c7788093afb82e2997b84806
gjack <gabi@gabijack.com>
2018-12-28 22:11:53 -0500

    Tweak Travis file. Add a matcher

--- lib/lita/handlers/whats_brad_eating.rb
+++ lib/lita/handlers/whats_brad_eating.rb
@@ -2,6 +2,16 @@ module Lita
   module Handlers
     class WhatsBradEating < Handler
       # insert handler code here
+      route /^what's brad eating$/i,
+      :brad_eats,
+      command: true,
+      help: {
+        "what's brad eating" => "latest post from brad's food tumblr"
+      }
+
+      def brad_eats(response)
+        response.reply 'Actual results coming soon!'
+      end
 
       Lita.register_handler(self)
     end
