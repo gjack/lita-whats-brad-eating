25d584681de8254ab7bd74559373b118178268ca
gjack <gabi@gabijack.com>
2019-01-03 08:03:42 -0500

    Pull html from Brads page

--- lib/lita/handlers/whats_brad_eating.rb
+++ lib/lita/handlers/whats_brad_eating.rb
@@ -2,6 +2,8 @@ module Lita
   module Handlers
     class WhatsBradEating < Handler
       # insert handler code here
+      BLOG_URL = 'https://whatsbradeating.tumblr.com'.freeze
+
       route /^what's brad eating$/i,
       :brad_eats,
       command: true,
@@ -11,6 +13,10 @@ module Lita
 
       def brad_eats(response)
         response.reply 'Actual results coming soon!'
+      end
+
+      def response
+        @_response ||= http.get(BLOG_URL)
       end
 
       Lita.register_handler(self)
--- spec/lita/handlers/whats_brad_eating_spec.rb
+++ spec/lita/handlers/whats_brad_eating_spec.rb
@@ -5,4 +5,20 @@ describe Lita::Handlers::WhatsBradEating, lita_handler: true do
     it { is_expected.to route("Lita what's brad eating") }
     it { is_expected.to route("Lita what's BRAD eating") }
   end
+
+  describe 'response' do
+    let(:body) { subject.response.body }
+
+    it "should be able to pull down some HTML from Brad's blog" do
+      expect(body =~ /<html>/i).to be_truthy
+    end
+
+    it 'should include something that looks like an image tag' do
+      expect(body =~ /img src/i).to be_truthy
+    end
+
+    it 'should include a caption' do
+      expect(body.include?('caption')).to be_truthy
+    end
+  end
 end
