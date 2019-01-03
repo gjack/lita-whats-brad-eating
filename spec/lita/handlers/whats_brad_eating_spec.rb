e479b22c7417b71082baeb36e4f0e77f6547a9de
gjack <gabi@gabijack.com>
2019-01-03 07:49:24 -0500

    Adding specs for the route

--- spec/lita/handlers/whats_brad_eating_spec.rb
+++ spec/lita/handlers/whats_brad_eating_spec.rb
@@ -1,4 +1,8 @@
 require "spec_helper"
 
 describe Lita::Handlers::WhatsBradEating, lita_handler: true do
+  describe 'routes' do
+    it { is_expected.to route("Lita what's brad eating") }
+    it { is_expected.to route("Lita what's BRAD eating") }
+  end
 end
